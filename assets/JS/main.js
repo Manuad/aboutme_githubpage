f8b98893cb9a62ba0f7a5155efb42495dd79b1a4
Manuad <manuel.adan4@gmail.com>
2022-04-27 12:17:57 -0500

    First commit

---
+++ assets/JS/main.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.headline');
+ScrollReveal().reveal('#imagenes', {delay: 550});
+ScrollReveal().reveal('#video', {delay: 550});
+ScrollReveal().reveal('#rowAdoptame', {delay: 550});
